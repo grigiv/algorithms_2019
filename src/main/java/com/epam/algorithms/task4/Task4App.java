2e670b7d3798864ef7171f580d087a2990c7d6a3
Ilia Grigorev <grigorieviv@gmail.com>
2019-06-17 18:44:22 +0300

    Use LinkedListPrototype to check if brackets in string are used correctly (Task #4)

---
+++ src/main/java/com/epam/algorithms/task4/Task4App.java
@@ -0,0 +1,54 @@
+package com.epam.algorithms.task4;
+
+import com.epam.algorithms.task1.LinkedListPrototype;
+
+import java.util.Scanner;
+
+public class Task4App {
+    public static void main(String[] args) {
+        boolean isBalanced = true;
+        Scanner scanner = new Scanner(System.in);
+        LinkedListPrototype linkedListPrototype = new LinkedListPrototype();
+        System.out.println(" Input a string, please!");
+        String inputString = scanner.nextLine();
+        label:
+        for (int i = 0; i < inputString.length(); i++) {
+            switch (inputString.charAt(i)) {
+                case '(':
+                    linkedListPrototype.addFirst(String.valueOf(inputString.charAt(i)));
+                    break;
+                case '{':
+                    linkedListPrototype.addFirst(String.valueOf(inputString.charAt(i)));
+                    break;
+                case '[':
+                    linkedListPrototype.addFirst(String.valueOf(inputString.charAt(i)));
+                    break;
+                case ')':
+                    if (linkedListPrototype.head() != null && linkedListPrototype.head().equals("(")) {
+                        linkedListPrototype.removeFirst();
+                    } else {
+                        isBalanced = false;
+                        break label;
+                    }
+                    break;
+                case '}':
+                    if (linkedListPrototype.head() != null && linkedListPrototype.head().equals("{")) {
+                        linkedListPrototype.removeFirst();
+                    } else {
+                        isBalanced = false;
+                        break label;
+                    }
+                    break;
+                case ']':
+                    if (linkedListPrototype.head() != null && linkedListPrototype.head().equals("[")) {
+                        linkedListPrototype.removeFirst();
+                    } else {
+                        isBalanced = false;
+                        break label;
+                    }
+                    break;
+            }
+        }
+        System.out.println("String is balanced: " + isBalanced);
+    }
+}
